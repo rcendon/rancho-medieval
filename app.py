23274bd2ebf75f39505c79fc5c64a98c03f38e19
danielhavilla <daniel.avilla@aluno.faculdadeimpacta.com.br>
2021-10-04 01:54:29 -0300

    Teste tela cardapio

--- app.py
+++ app.py
@@ -15,19 +15,22 @@
 #git clone https://github.com/rcendon/rancho-medieval.git .
 #
 #
-################################   Servidor Flask    ###############################################
+################################   Servidor Flask    ####################################################
 
 #Importar o flask e do objeto Flask importar o render_template eo redirect 
 #from re import template
 from flask import Flask, render_template, redirect, request, url_for 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import DOUBLE_PRECISION
 
 #Construir APP - app recebe o objeto Flask (Instância do objeto Flask)
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hkuxpjcwuldatj:8a0d2ed471b0e35e8aa4b8d123186db7c263dc22c6a49e33fc86578ea91bd660@ec2-44-195-201-3.compute-1.amazonaws.com:5432/dc60qmfkulhdc5'
 db = SQLAlchemy(app) #db recebe o app Flask para automatização.
+db_cardapio = SQLAlchemy(app) #db_cardapio recebe o app Flask para automatização.
 
-################################ Modelo Pessoas ######################################
+################################ Modelo Pessoas ##########################################################
+#########################################################################################################
 
 class Pessoas(db.Model): #Modelo Pessoas
    __tablename__ = 'pessoas' 
@@ -50,9 +53,9 @@ class Pessoas(db.Model): #Modelo Pessoas
        #self.endereco = endereco
        self.senha = senha
 
-#######################################################################################
+########################################################################################################
 
-#Create
+#Create Pessoas
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST': #SE POST
@@ -68,28 +71,57 @@ def add():
         return redirect(url_for('index')) #Se o metodo POST for OK retornar para o INDEX
     return render_template('add.html') #ELSE mostra pagina ADD
 
-#######################################################################################
+########################################################################################################
 
-#Update
+#Update Pessoas
 
-#######################################################################################
+########################################################################################################
 
-#delete
+#Delete Pessoas 
 
-#######################################################################################
+############################### Fim Modelo Pessoas #####################################################
+########################################################################################################
+
+
+
+################################ Modelo Cardápio ########################################################
+#########################################################################################################
+
+class Cardapio(db_cardapio.Model):
+   __tablename__ = 'cardapio' 
+    
+   id_produto = db_cardapio.Column(db_cardapio.Integer, autoincrement=True, primary_key=True)
+   nome = db_cardapio.Column(db_cardapio.VARCHAR(50), nullable=False, unique=True)
+   quantidade_estoque_produto = db_cardapio.Column(db_cardapio.VARCHAR(10), nullable=False)
+   permite_estocagem = db_cardapio.Column(db.Boolean)
+   valor = db_cardapio.Column(DOUBLE_PRECISION)
+   
+   def __init__(self, nome, quantidade_estoque_produto, permite_estocagem, valor):
+       self.nome = nome
+       self.quantidade_estoque_produto = quantidade_estoque_produto
+       self.permite_estocagem = permite_estocagem
+       self.valor = valor
+
+############################### Fim Modelo Cardápio #####################################################
+########################################################################################################
 
 #Rota para renderizar a pagina
 @app.route('/')
 #Função da Rota
 def index():
-    pessoas = Pessoas.query.all() #Select * from
-    return render_template('index.html', pessoas=pessoas)
+    pessoas = Pessoas.query.all() #Select * from Pessoas
+    cardapio = Cardapio.query.all() #Select * from Cardapio
+
+    return render_template('index.html', pessoas=pessoas, cardapio=cardapio)
+
+    
 
 #Para aumentar a segurança o app.run() só roda se ele estiver no arquivo principal 
 #if __name__ == '__main__': 
 db.create_all()
+db_cardapio.create_all()
 app.run(debug=True) #Roda o aplicativo 
     # Obs: debug=True Modo desenvolvedor para atualizar os templates automaticamente.
 
-##################################################################################################
+########################################################################################################
 
--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-/* Animação menu modo Mobile */
+/* ################# Animação menu modo Mobile #########################################*/
 /* querySelector() = retorna o primeiro elemento dentro do documento */
 
 /* Constante para chamar o botão menu mobile / ( <i class="bi bi-list menu-mobile"></i> )  */
@@ -16,3 +16,22 @@ menuMobile.addEventListener('click', () => {
 
     body.classList.toggle("menu-nav-active") /*Adiciona uma classe nova a minha tag body chamada menu-nav-active- style.css */
 })
+
+
+/* ################# Quantidade Cardápio #########################################*/
+function id( el ){
+    return document.getElementById( el );
+}
+window.onload = function(){
+    id('mais').onclick = function(){
+            id('format').value = parseInt( id('format').value )+1;
+            
+            id('total').value = 20*id('format').value;
+    }
+    id('menos').onclick = function(){
+            if( id('format').value>0 )
+                    id('format').value = parseInt( id('format').value )-1;
+            
+            id('total').value = 20*id('format').value;
+    }
+} 
