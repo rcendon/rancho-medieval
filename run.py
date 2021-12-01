07b23cb96ced96343f587f2c04ba9c6fc87b9b22
rcendon <cendon.r@outlook.com>
2021-11-30 23:07:26 -0300

    atualiza o arquivo run.py

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ import flask
 
 from app import app
 
-#if __name__ == "__main__":
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
 # Obs: debug=True Modo desenvolvedor para atualizar os templates automaticamente.
 
 
